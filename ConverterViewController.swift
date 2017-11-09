abe93cda4855295dec1d95d999af770ff619277a
Zach Hogan <zrhkc7@mail.missouri.edu>
2017-11-09 12:05:37 -0600

    Uploaded files since git push/commit is not working again

---
+++ ConverterViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ConverterViewController.swift
+//  ConversionCalculator
+//
+//  Created by Zach Hogan on 11/7/17.
+//  Copyright © 2017 Tech Innovator. All rights reserved.
+//
+
+import UIKit
+
+class ConverterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var outputDisplay: UITextField!
+    @IBOutlet weak var inputDisplay: UITextField!
+    
+    @IBAction func converterDisplay(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
